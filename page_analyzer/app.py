07e7a898d9a66e33f86df0f41c996cc0fc7583e7
Sergei Mikurov <454004@mail.ru>
2023-06-18 01:33:19 +0500

    add templates

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -3,6 +3,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.get('/')
 def index():
     return render_template('index.html')
+
+
+@app.get('/urls')
+def get_urls():
+    return render_template('urls.html')
+
+
+@app.get('/report')
+def get_report():
+    return render_template('report.html')
+
+
+@app.post('/report')
+def post_report():
+    return render_template('report.html')
