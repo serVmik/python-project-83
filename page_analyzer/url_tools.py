be216e3fa3d910c3c9268453cc77c6178b7594ce
Sergei Mikurov <454004@mail.ru>
2023-07-02 22:25:55 +0500

    Revert "Revert "refactoring""
    This reverts commit 72802ac5ba6e5102e6d8a9dd92cc4b6b6d8e93d3.

--- page_analyzer/url_tools.py
+++ page_analyzer/url_tools.py
@@ -4,26 +4,30 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def normalize_url(url):
-    parsed_url = urlparse(url)
+def normalize_url(entered_url):
+    parsed_url = urlparse(entered_url)
     return f'{parsed_url.scheme}://{parsed_url.netloc}'
 
 
-def check_url_for_errors(url, norm_url):
+def check_url_for_errors(entered_url, url):
     url_error = []
-
-    if len(url) > 255:
+    if len(entered_url) > 255:
         url_error.append(('URL превышает 255 символов', 'danger'))
-    if url == '':
+    if entered_url == '':
         url_error.append(('URL обязателен', 'danger'))
-    if not validators.url(norm_url):
+    if not validators.url(url):
         url_error.append(('Некорректный URL', 'danger'))
-
     return url_error
 
 
-def get_requests_info(norm_url):
-    r = requests.get(norm_url)
+def get_requests_info(url):
+    try:
+        r = requests.get(url)
+    except requests.ConnectionError:
+        return None
+    if r.status_code != 200:
+        return None
+
     requests_info = {'status_code': r.status_code}
 
     soup = BeautifulSoup(r.text, 'html.parser')
