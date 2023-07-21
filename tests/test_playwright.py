42b6ab9f7718a71db267c20de61f9cd3448e41b6
Sergei Mikurov <454004@mail.ru>
2023-07-22 03:25:13 +0500

    fix http://127.0.0.1:8001 in test_playwright.py
    fix http://0.0.0.0:8001 in test_playwright.py

--- tests/test_playwright.py
+++ tests/test_playwright.py
@@ -3,7 +3,7 @@ from playwright.sync_api import Page, expect
 
 
 def test_homepage(page: Page):
-    page.goto('http://127.0.0.1:5000')
+    page.goto('http://127.0.0.1:8001')
 
     # Ожидайте, что заголовок "содержит" подстроку.
     # <title>Анализатор страниц</title>
