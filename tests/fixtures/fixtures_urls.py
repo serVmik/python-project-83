17cc922c7f12807fefa9556d736537c4527458bd
Sergei Mikurov <454004@mail.ru>
2023-07-23 17:02:18 +0500

    fix test_urls.py

--- tests/fixtures/fixtures_urls.py
+++ tests/fixtures/fixtures_urls.py
@@ -8,7 +8,7 @@ urls_incorrect = ('htp://sorry.jo', 'http;//just_do.it', 'http://benq,ru')
 
 
 def give_one_by_one(func):
-    def wrapper(urls):
-        for url in urls:
-            func(url)
+    def wrapper(*args):
+        for arg in args:
+            func(arg)
     return wrapper
--- tests/test_urls.py
+++ tests/test_urls.py
@@ -1,26 +1,36 @@
 import pytest
 
-from page_analyzer.urls import validate_url
-from tests.fixtures.fixtures_urls import (give_one_by_one,
-                                          urls_incorrect, url_too_long)
+from page_analyzer.urls import normalize_url, validate_url
+from tests.fixtures.fixtures_urls import (
+    give_one_by_one,
+    urls_incorrect, url_too_long)
 
 
-def test_url_empty():
-    (message, _) = validate_url('')
-    assert message == ('URL обязателен', 'danger')
+@pytest.mark.parametrize('url_entered, url_name', [
+    ('https://ru.hexlet.io/courses/python-basics',
+     'https://ru.hexlet.io'),
+    ('https://github.com/serVmik/python-project-83',
+     'https://github.com'),
+    ('https://translate.google.com/?hl=ru&sl=ru&tl=en&op=translate',
+     'https://translate.google.com')])
+def test_normalize_url(url_entered, url_name):
+    url_parsed = normalize_url(url_entered)
+    assert url_name == url_parsed
 
 
-def test_url_too_long():
-    (message, _) = validate_url(url_too_long)
-    assert message == ('URL превышает 255 символов', 'danger')
+def test_url_empty_and_too_long():
+    (message_empty, _) = validate_url('')
+    (message_too_long, _) = validate_url(url_too_long)
+    assert message_empty == ('URL обязателен', 'danger')
+    assert message_too_long == ('URL превышает 255 символов', 'danger')
 
 
 @pytest.fixture
-def urls():
+def urls_incorrect_():
     return urls_incorrect
 
 
 @give_one_by_one
-def test_url_incorrect(urls):
-    [(message, _)] = validate_url(urls)
-    assert message == 'Некорректный URL'
+def test_url_incorrect(urls_incorrect_):
+    (message) = validate_url(urls_incorrect_)
+    assert message == ('Некорректный URL', 'danger')
