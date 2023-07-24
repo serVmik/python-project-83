069c9286bc6e125d9ba57bf6c2be0c2f97f06f42
Sergei Mikurov <454004@mail.ru>
2023-07-24 12:27:17 +0500

    fix test_by_playwright.py

--- tests/test_by_playwright.py
+++ tests/test_by_playwright.py
@@ -5,8 +5,8 @@ with sync_playwright() as p:
     page = browser.new_page()
 
     page.goto('http://127.0.0.1:5000')
-    page.screenshot(path='tests/fixtures/screenshots/index_1.png')
     page.get_by_role('textbox').fill('https://ru.hexlet.io/my')
+    page.screenshot(path='tests/fixtures/screenshots/index_1.png')
     page.get_by_role('button').click()
 
     page.screenshot(path='tests/fixtures/screenshots/urls_1.png')
